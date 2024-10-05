54b426de93a8593c0a487eddff9460c439df1ae8
DESKTOP-BCBFLT7\Alex <alexvall24@gmail.com>
2024-10-05 15:23:00 +0200

    Initial commit

---
+++ src/test/java/com/example/telegram_summary_ai/TelegramSummaryAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegram_summary_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramSummaryAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
